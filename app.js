03f378e5ca6a66c038d7d6722cf1d414b4e7941b
wchang28 <wchang28@hotmail.com>
2018-01-03 17:16:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+var http = require('http');
+var https = require('https');
+var fs = require("fs");
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+var bpj = bodyParser.json({"limit":"999mb"});   // json body middleware
+app.use(bpj);
+
+app.use(function(req, res, next) {
+	console.log('**********************************************************************');
+	console.log('incoming request from ' + req.connection.remoteAddress + ":" + req.connection.remotePort + ', url='+ req.url);
+	console.log('method=' + req.method);
+	console.log('headers: ' + JSON.stringify(req.headers));
+	console.log('**********************************************************************');
+	console.log('');
+	next();
+});
+
+//app.set("json replacer", null);
+//app.set("json spaces", 2);
+
+app.get('/', function(req, res) {
+	res.json({msg: 'Great! It works'});
+});
+
+app.get('/test', function(req, res) {
+	res.json({msg: 'good morning'});
+});
+
+app.post('/test', function(req, res) {
+	res.json(req.body);
+});
+
+app.put('/test_put', function(req, res) {
+	res.json({});
+});
+
+var certificate_file = path.resolve(__dirname, "./server-pem.crt");
+var private_key_file = path.resolve(__dirname, "./server-private-key.pem");
+
+var certificate = fs.readFileSync(certificate_file, 'utf8');
+var privateKey  = fs.readFileSync(private_key_file, 'utf8');
+
+let credentials = {
+	cert: certificate
+	,key: privateKey
+	,requestCert: false
+    ,rejectUnauthorized: false
+};
+
+var config = {
+	host: "0.0.0.0"
+	,port: 443
+};
+
+var server = https.createServer(credentials, app);
+
+var port = config.port;
+var host = config.host;
+
+server.listen(port, host, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	 console.log('app server listening at %s://%s:%s', 'https', host, port);
+});
---
+++ test.js
@@ -0,0 +1,30 @@
+//const crypto = require('crypto');
+//const ciphers = crypto.getCiphers();
+//console.log(JSON.stringify(ciphers, null, 2)); // ['aes-128-cbc', 'aes-128-ccm', ...]
+
+var request = require("request");
+var path = require('path');
+var fs = require("fs");
+
+/*
+request.get('https://localhost', {strictSSL: false}, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+*/
+
+var server_ca_file = path.resolve(__dirname, "./rootCA-pem.crt");
+
+var options = {
+	agentOptions: {
+		ca: [
+			fs.readFileSync(server_ca_file)
+		]
+	}
+}
+request.get('https://localhost', options, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
